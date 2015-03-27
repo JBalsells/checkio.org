b6718e181fe1fe6b8bb3c7bdaa07ad8a7a29677c
Jorge Balsells <jorge_balsells@hotmail.com>
2015-03-27 00:10:13 -0600

    Update Letter-Queue.py

--- Electronic Station/Letter-Queue.py
+++ Electronic Station/Letter-Queue.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 21:47:49 2015
-
-@author: jorge_000
-"""
-
+def letter_queue(commands):
+    x=[]
+    Str=""
+    l=0
+    for i in commands:
+        if i[1]=='U':
+            x.append(i[5])
+        if i[1]=='O':
+            if len(x)>0:
+                del x[0]
+    for i in x:
+        Str+=i
+    return Str
